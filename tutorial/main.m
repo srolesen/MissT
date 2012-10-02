d4a0cb89228dd7bb5403e84c0bde0b4884fea26c
Simon Olesen <simonolesen@Simon-Olesens-MacBook-Pro-15.local>
2012-10-02 14:48:40 +0200

    Initial Commit

---
+++ tutorial/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  tutorial
+//
+//  Created by Simon Olesen on 02/10/12.
+//  Copyright (c) 2012 Simon Olesen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
